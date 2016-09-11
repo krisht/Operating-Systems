fdd02305608c31e92856cb3d143a2d73ae9838f5
Krishna Thiyagarajan <krisht@cooper.edu>
2016-09-11 01:20:41 -0400

    Changed things in copycat.c

--- copycat.c
+++ copycat.c
@@ -1,72 +1,63 @@
-
-#include <iostream>
-
 #include <ctype.h>
 #include <stdio.h>
 #include <err.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <stdlib.h> //contains exit(rval)
 #include <unistd.h> //contains getopt
-using namespace std;
 
-int bFlag, oFlag;
 char *oValue = NULL;
-int bValue = 0; 
-int rval; 
+int bValue = -1;
 
-static void usage(void);
-void processFiles(const int inputStart, int argc, char** argv); 
+/**
+* Processes files according to description given the following
+* @param inputStart start index of the input files
+* @param argc       count of arguments supplied
+* @param argv       values of the arguments supplied
+*/
+void processFiles(const int inputStart, int argc, char** argv){
+
+	const char* oFileName = (oValue == NULL) ? "stdout" : oValue; 
+	char* iFileName; 
+	int ifd, ofd; 
+	char data[1028]; 
+
+
+	if(inputStart == argc){
+		ifd = fileno(stdin); 
+		ofd = fileno(stdout);
+		read(ifd, data, 1028); 
+		printf("%s\n", data); 
+	}
+	else{
+		for(int i = inputStart; i < argc; i++){
+			ifd = open(argv[i], O_RDONLY);
+			read(ifd, data, 1); 
+			printf("%s\n", data);  
+			printf("Input: %s\n", argv[i]);
+			printf("Input: %d\n", ifd); 
+		}
+	}
+}
 
 int main(int argc, char** argv){
-
 	int ch; 
-
-	opterr = 0; 
-
-	while((ch=getopt(argc, argv, "b:o:")) != -1){
+	while((ch = getopt(argc, argv, "b:o:")) != -1)
 		switch(ch){
-			case 'b': 	bFlag=1; 
-						bValue = atoi(optarg);
-						break;
-			case 'o': 	oFlag = 1;
-						oValue = optarg; 
-						break;
-			case '?':	usage(); 
+			case 'b': 	bValue = atoi(optarg); 		break;
+			case 'o': 	oValue = optarg;			break;
+			case '?': 	fprintf(stderr, "usage: copycat [-b ###] [-o outfile] [infile1 infile2 ...]\n");
 						exit(1); 
 						break; 
 			default : abort(); 
 		}
 
-	}
-
 	processFiles(optind, argc, argv); 
 
 	if(fclose(stdout))
 		err(1, "stdout"); 
-	exit(rval); 
+	exit(0); 
 }
 
-void processFiles(const int inputStart, int argc, char** argv){
-/*	
-	const char* oFileName = oValue; 
-	char* iFileName; 
-	int ifd, ofd; 
-
-
-	if(inputStart == argc){
-		oFileName = "stdout"; 
-		ifd = STDIN_FILENO;
-	}*/
-
-	printf("B Value: %d\n",  bValue); 
-	printf("O Value: %s\n", oValue); 
-
-	for(int i = inputStart; i < argc; i++)
-		printf("Input: %s\n", argv[i]); 
-}
-
-static void
-usage(void)
-{
-	fprintf(stderr, "usage: copyccat [-b ###] [-o outfile] [infile1 infile2 ...]\n");
-	exit(1);
-}
