85e9b4406b195b46d8ab5759f5d3b06e12976cb1
Krishna Thiyagarajan <krisht@cooper.edu>
2016-09-11 18:40:02 -0400

    Changed formatting a bit. Made code prettier

--- copycat.c
+++ copycat.c
@@ -5,66 +5,76 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFSIZE 1024
+#define DFLT_BUF_SIZE 1024
 
 char *oValue = NULL;
 int bValue = -1;
 
-void sysCallFiles(const char* inputFile, int ofd ){
+void sysCallFiles(const char *inputFile, int ofd) {
     int ifd = (!strcmp(inputFile, "stdin")) ? fileno(stdin) : open(inputFile, O_RDONLY);
-    int len;  
-	if(bValue == -1){
-		char data[BUFSIZE]; 
-        while((len = read(ifd, data, BUFSIZE)) > 0)
+    int len;
+    if (bValue == -1) {
+        char data[DFLT_BUF_SIZE];
+        while ((len = read(ifd, data, DFLT_BUF_SIZE)) > 0)
             write(ofd, data, len);
-	}
-	else{
+    }
+    else {
         char data[bValue];
-        int len = read(ifd, data, bValue); 
+        len = read(ifd, data, bValue);
         write(ofd, data, len);
-	}
-    write(ofd, "\n", 1); 
-    close(ifd); 
+    }
+    write(ofd, "\n", 1);
+    close(ifd);
+
 }
 
-void processFiles(const int inputStart, int argc, char** argv){
+void processFiles(const int inputStart, int argc, char **argv) {
 
-    const char* oFileName = (oValue == NULL) ? "stdout" : oValue;
+    const char *oFileName = (oValue == NULL) ? "stdout" : oValue;
     int ofd = (!strcmp(oFileName, "stdout")) ? fileno(stdout) : open(oFileName, O_WRONLY | O_CREAT | O_APPEND, 0666);
-    const char* iFileName = (inputStart == argc) ? "stdin" : "";
+    const char *iFileName = (inputStart == argc) ? "stdin" : "";
 
     printf("output: %s\n", oFileName);
 
-    if(!strcmp(iFileName, "stdin")){
+    if (!strcmp(iFileName, "stdin")) {
         printf("input: stdin\n");
         sysCallFiles("stdin", ofd);
     }
-    else
-        for(int i = inputStart; i < argc; i++){
-            if(!strcmp(argv[i], "-")){
+    else {
+        int kk;
+        for (kk = inputStart; kk < argc; kk++) {
+            if (!strcmp(argv[kk], "-")) {
                 printf("input: stdin\n");
-                sysCallFiles("stdin", ofd);
+                sysCallFiles("stdin", ofd); //why isn't it syscalling again when "-" is indicated
             }
-            else{
-                printf("input: %s\n",argv[i]);
-                sysCallFiles(argv[i], ofd);
+            else {
+                printf("input: %s\n", argv[kk]);
+                sysCallFiles(argv[kk], ofd);
             }
         }
+    }
+    close(ofd);
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
     int ch;
-    while((ch = getopt(argc, argv, "b:o:")) != -1)
-        switch(ch){
-            case 'b': 	bValue = atoi(optarg); 		break;
-            case 'o': 	oValue = optarg;			break;
-            case '?': 	exit(1); //fprintf(stderr, "usage: copycat [-b ###] [-o outfile] [infile1 infile2 ...]\n");
-            default : abort();
+    while ((ch = getopt(argc, argv, "b:o:")) != -1)
+        switch (ch) {
+            case 'b':
+                bValue = atoi(optarg);
+                break; //Check whether optarg is a number
+            case 'o':
+                oValue = optarg;
+                break;
+            case '?':
+                exit(1); //fprintf(stderr, "usage: copycat [-b ###] [-o outfile] [infile1 infile2 ...]\n");
+            default :
+                abort();
         }
 
     processFiles(optind, argc, argv);
 
-    if(fclose(stdout))
+    if (fclose(stdout))
         err(1, "stdout");
     exit(0);
 }
