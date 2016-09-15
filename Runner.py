140c04e1f4a7ae7ffdbbc5a13864dd232458fc6a
Krishna Thiyagarajan <krisht@cooper.edu>
2016-09-15 01:23:14 -0400

    Added *.py program. Got results

---
+++ Runner.py
@@ -0,0 +1,14 @@
+import os; 
+import time; 
+import csv; 
+
+out = [j for j in range(0,19)];
+
+with open('result.csv', 'wb') as csvfile:
+	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL);
+	for jj in out:
+		for kk in range(0, 10): 
+			start = time.time(); 
+			os.system("./copycat -b %d -o output.txt input.txt input1.txt input2.txt input3.txt input4.txt" % 2**jj)
+			end = time.time(); 
+			spamwriter.writerow([2**jj, jj ,  (end-start)]);  
