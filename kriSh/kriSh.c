2ad60497800f7f791363a2ab56462d2f2260b0c1
Krishna Thiyagarajan <krisht@cooper.edu>
2016-10-19 17:02:56 -0400

    Edited kriSh.c

--- kriSh/kriSh.c
+++ kriSh/kriSh.c
@@ -10,7 +10,7 @@
 #include <sys/wait.h>
 #include <stdarg.h>
 
-void specialExit(const char *retVal, const char *format, ...) {
+void specialExit(const char* retVal, const char* format, ...) {
     va_list arg;
     va_start (arg, format);
     vfprintf(stderr, format, arg);
@@ -21,15 +21,15 @@ void specialExit(const char *retVal, const char *format, ...) {
 
 }
 
-char **getArgs(char *line, int *count) {
-    char **args = NULL;
-    char *delimiters = "\t ";
+char** getArgs(char* line, int *count) {
+    char** args = NULL;
+    const char* delimiters = "\t ";
     line[strlen(line) - 1] = 0;
 
-    char *arg = strtok(line, delimiters);
+    char* arg = strtok(line, delimiters);
 
     while (arg != NULL) {
-        args = realloc(args, ++(*count) * sizeof(char *));
+        args = (char** )realloc(args, ++(*count) * sizeof(char* ));
         if (args == NULL)
             specialExit("1", "Error reallocating args memory.\n");
 
@@ -40,11 +40,11 @@ char **getArgs(char *line, int *count) {
 }
 
 // Returns amount of arguments excluding redirections
-int getCmdArgC(char **args, int count) {
-    int i;
+int getCmdArgC(char** args, int count) {
+    int kk;
     int cmdArgC = 0;
-    for (i = 0; i < count; ++i) {
-        char *a = args[i];
+    for (kk = 0; kk < count; kk++) {
+        char* a = args[kk];
         if (!(a[0] == '<' || a[0] == '>' || (a[0] == '2' && a[1] == '>')))
             cmdArgC++;
     }
@@ -52,126 +52,122 @@ int getCmdArgC(char **args, int count) {
 }
 
 // Returns amount of redirection arguments
-int getRedirArgC(char **args, int count) {
-    int i;
+int getRedirArgC(char** args, int count) {
+    int kk;
     int redirArgC = 0;
-    for (i = 0; i < count; ++i) {
-        char *a = args[i];
+    for (kk = 0; kk < count; kk++) {
+        char* a = args[kk];
         if ((a[0] == '<' || a[0] == '>' || (a[0] == '2' && a[1] == '>')))
             redirArgC++;
     }
     return redirArgC;
 }
 
-int forkProcess(int redirArgC, char **redirArgV, char **rawArgV, char **cmdArgV) {
-
-
+int forkProcess(int redirArgC, char** redirArgV, char** rawArgV, char** cmdArgV) {
+    //Set up timers
     struct timeval startTime, endTime;
-    if (gettimeofday(&startTime, NULL) < 0)
-        specialExit("1", "Error getting start time: %s\n", strerror(errno));
-
-    pid_t pid;
     struct rusage ru;
     int status;
 
-    switch (pid = fork()) {
+    if (gettimeofday(&startTime, NULL) < 0)
+        specialExit("1", "Error getting start time: %s\n", strerror(errno));
+
+    switch (fork()) {
         case 0: {
-            int redi, fd, fileNum;
-            for (redi = 0; redi < redirArgC; ++redi) {
-                char *fileName = NULL;
-                if (redirArgV[redi][0] == '<') {
-                    // Redirect stdin
-                    fileName = redirArgV[redi];
-                    ++fileName;
+            int kk, fd, fileNum;
+            for (kk = 0; kk < redirArgC; kk++) {
+                char* fileName = NULL;
+                if (redirArgV[kk][0] == '<') { // Redirect STDIN
+                    fileName = redirArgV[kk] + 1;
                     fd = open(fileName, O_RDONLY);
                     fileNum = STDIN_FILENO;
-                } else if (redirArgV[redi][0] == '>' && redirArgV[redi][1] != '>') {
-                    // Redirect stdout (Open/Create/Truncate)
-                    fileName = redirArgV[redi];
-                    ++fileName;
+                }
+                else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] != '>') { //Redirect STDOUT (open/creat/trunc)
+                    fileName = redirArgV[kk] + 1;
                     fd = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDOUT_FILENO;
-                } else if (redirArgV[redi][0] == '2' && redirArgV[redi][1] == '>') {
-                    // Redirect stderr (Open/Create/Truncate)
-                    fileName = redirArgV[redi];
-                    fileName += 2;
+                }
+                else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>') { //Redirect STDERR (open/creat/trunc)
+                    fileName = redirArgV[kk] + 2;
                     fd = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDERR_FILENO;
-                } else if (redirArgV[redi][0] == '>' && redirArgV[redi][1] == '>') {
-                    // Redirect stdout (Open/Create/Append)
-                    fileName = redirArgV[redi];
-                    fileName += 2;
+                }
+                else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] == '>') { //Redirect STDOUT (open/creat/append)
+                    fileName = redirArgV[kk] + 2;
                     fd = open(fileName, O_WRONLY | O_APPEND | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDOUT_FILENO;
-                } else if (redirArgV[redi][0] == '2' && redirArgV[redi][1] == '>' && redirArgV[redi][2] == '>') {
+                }
                     // Redirect stderr (Open/Create/Append)
-                    fileName = redirArgV[redi];
-                    fileName += 3;
+                else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>' && redirArgV[kk][2] == '>') {
+                    fileName = redirArgV[kk] + 3;
                     fd = open(fileName, O_WRONLY | O_APPEND | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDERR_FILENO;
                 }
+
                 if (fd < 0)
-                    specialExit("1", "Can't open file %s: %s\n", fileName, strerror(errno));
+                    specialExit("1", "File %s cannot be opened: %s\n", fileName, strerror(errno));
 
                 // Dup files and close dangling descriptors
                 if (dup2(fd, fileNum) < 0)
-                    specialExit("1", "Can't dup2 %s to fd %d: %s\n", fileName, fileNum, strerror(errno));
+                    specialExit("1", "Error with dup2(%d, %s): %s\n", fileNum, fileName, strerror(errno));
+
                 close(fd);
             }
 
-            execvp(rawArgV[0], cmdArgV);
+            execvp(rawArgV[0], cmdArgV); //execute command
             free(rawArgV);
             break;
 
         }
         case -1:
-            specialExit("1""Fork failed: %s\n", strerror(errno));
+            specialExit("1", "Fork failed: %s\n", strerror(errno));
             break;
         default:
             if (wait3(&status, 0, &ru) < 0)
-                specialExit("1", "Wait3 error: %s\n", strerror(errno));
+                specialExit("1", "Error with wait3: %s\n", strerror(errno));
 
             if (gettimeofday(&endTime, NULL) < 0)
-                specialExit("1", "Error getting end time: %s\n", strerror(errno));
+                specialExit("1", "Error with getting end time: %s\n", strerror(errno));
 
-            int realsecdiff = endTime.tv_sec - startTime.tv_sec;
-            int realmicrodiff = endTime.tv_usec - startTime.tv_usec;
+            int realsecdiff = (int)(endTime.tv_sec - startTime.tv_sec);
+            int realmicrodiff = (int)(endTime.tv_usec - startTime.tv_usec);
             printf("Command returned with return code %d,\n", WEXITSTATUS(status));
-            printf("consuming %01d.%03d real seconds, %01d.%03d user, %01d.%03d system.\n",
-                   realsecdiff, realmicrodiff, ru.ru_utime.tv_sec, ru.ru_utime.tv_usec, ru.ru_stime.tv_sec,
-                   ru.ru_stime.tv_usec);
+            printf("consuming %01d.%03d real, %01d.%03d user, %01d.%03d system seconds.\n", realsecdiff, realmicrodiff, (int)ru.ru_utime.tv_sec, (int)ru.ru_utime.tv_usec, (int)ru.ru_stime.tv_sec, (int)ru.ru_stime.tv_usec);
 
     }
 }
 
-int execCmd(char *line, char **argv, int argc) {
+void execCmd(char* line) {
     int count = 0;
-
-    char **rawArgV = getArgs(line, &count);
+    char** rawArgV = getArgs(line, &count);
     int cmdArgC = getCmdArgC(rawArgV, count);
+    char** cmdArgV = (char** )malloc(cmdArgC * sizeof(char* ));
 
-    char **cmdArgV = malloc(cmdArgC * sizeof(char *));
     if (cmdArgC > 0) {
         int kk;
-        for (kk = 0; kk <= cmdArgC; ++kk)
-            cmdArgV[kk] = rawArgV[kk]; // Add add arg to commandArgs
+        for (kk = 0; kk <= cmdArgC; kk++)
+            cmdArgV[kk] = rawArgV[kk]; // Add arg to cmdArgV
 
-        cmdArgV = realloc(cmdArgV, (cmdArgC + 1) * sizeof(char *));
+        cmdArgV = (char**) realloc(cmdArgV, (cmdArgC + 1)* sizeof(char*));
         cmdArgV[cmdArgC] = 0; //Add null terminator
     }
 
     int redirArgC = getRedirArgC(rawArgV, count);
-    char **redirArgV = malloc(redirArgC * sizeof(char *));
+    char** redirArgV = (char** )malloc(redirArgC * sizeof(char* ));
     if (redirArgC > 0) {
         int kk;
         for (kk = 0; kk <= redirArgC; kk++)
-            redirArgV[kk] = rawArgV[kk +
-                                    cmdArgC]; //Add another arg to redirArgV but offset by cmdArgC to jump past cmd args
+            redirArgV[kk] = rawArgV[kk + cmdArgC];
+
+        //Add another arg to redirArgV but offset by cmdArgC to jump past cmd args
     }
 
     //Display info about command exec
 
-    printf("Executing command %s with arguments\"", rawArgV[0]);
+    if(rawArgV == NULL)
+        return;
+
+    printf("Executing command %s with arguments \"", rawArgV[0]);
 
     if (cmdArgC > 0) {
         int kk;
@@ -200,24 +196,28 @@ int execCmd(char *line, char **argv, int argc) {
     free(rawArgV);
     free(cmdArgV);
     free(redirArgV);
-    return 0;
+    return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     FILE *fp;
-    char *line = NULL;
+    char* line = NULL;
     size_t len = 0;
 
+    //Reads from stdin or from file with kriSh script
     if (argc == 1)
         fp = stdin;
     else fp = fopen(argv[1], "r");
+    printf("\nkriSh $ ");
 
     while (getline(&line, &len, fp) != -1) {
-        if (line[0] == '#')
+        if (line[0] == '#') //gets rid of comments
             continue;
-        execCmd(line, argv, argc);
+        if(!strcmp(line, "exit\n"))
+            break;
+        execCmd(line); //executes each command
+        printf("\nkriSh $ ");
     }
-
     free(line);
     exit(0);
 }
