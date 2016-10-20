1d2a9cb4b74d53fa1955e11aa2c6fa5c4b208a27
krisht <krisht@cooper.edu>
2016-10-20 00:54:09 -0400

    Fixed up a bunch of things. Added comments

--- kriSh/kriSh.c
+++ kriSh/kriSh.c
@@ -10,7 +10,7 @@
 #include <sys/wait.h>
 #include <stdarg.h>
 
-#define DEBUG_MODE 0
+#define DEBUG_MODE 1
 
 //Error reporting and exit function
 void specialExit(const char *retVal, const char *format, ...) {
@@ -68,97 +68,80 @@ int getRedirArgC(char **args, int count) {
 
 //Forking function that forks given certain parameters
 int forkProcess(int redirArgC, char **redirArgV, char **rawArgV, char **cmdArgV) {
-    //Set up timers
+    //Declares timers, resource usages and exit status
     struct timeval startTime, endTime;
     struct rusage resUsage;
-    int status;
+    int exitStat;
 
     if (gettimeofday(&startTime, NULL) < 0)
         specialExit("1", "Error getting start time: %s\n", strerror(errno));
 
-    pid_t pid = fork();
-
-    switch (pid) {
+    switch (fork()) {
         case 0: {
-            // Resolve I/O redirection
+            // I/O Redirection
             int kk, fd, fileNum;
-            printf("%s", redirArgV[0]);
-            for (kk = 0; kk < redirArgC; ++kk) {
+            for (kk = 0; kk < redirArgC; kk++) {
                 char *fileName = NULL;
-                if (redirArgV[kk][0] == '<') {
-                    // Redirect stdin
-                    fileName = redirArgV[kk];
-                    ++fileName;
+                if (redirArgV[kk][0] == '<') { //Redirect STDIN
+                    fileName = redirArgV[kk] + 1; //Offset to find filename is 1
                     fd = open(fileName, O_RDONLY);
                     fileNum = STDIN_FILENO;
-                } else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] != '>') {
-                    // Redirect stdout (Open/Create/Truncate)
-                    fileName = redirArgV[kk];
-                    ++fileName;
+                } else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] != '>') { // Redirect stdout (open/creat/trunc)
+                    fileName = redirArgV[kk] + 1;
                     fd = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDOUT_FILENO;
-                } else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>') {
-                    // Redirect stderr (Open/Create/Truncate)
-                    fileName = redirArgV[kk];
-                    fileName += 2;
+                } else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>' && redirArgV[kk][2] != '>') { // Redirect stderr (open/creat/trunc)
+                    fileName = redirArgV[kk] + 2; //Offset to find filename is 2
                     fd = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDERR_FILENO;
-                } else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] == '>') {
-                    // Redirect stdout (Open/Create/Append)
-                    fileName = redirArgV[kk];
-                    fileName += 2;
+                } else if (redirArgV[kk][0] == '>' && redirArgV[kk][1] == '>') {//Redirect stdout (open/creat/append)
+                    fileName = redirArgV[kk] + 2;
                     fd = open(fileName, O_WRONLY | O_APPEND | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDOUT_FILENO;
-                } else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>' && redirArgV[kk][2] == '>') {
-                    // Redirect stderr (Open/Create/Append)
-                    fileName = redirArgV[kk];
-                    fileName += 3;
+                } else if (redirArgV[kk][0] == '2' && redirArgV[kk][1] == '>' && redirArgV[kk][2] == '>') { //Redirect stderr (open/creat/append)
+                    fileName = redirArgV[kk] + 3; //Offset to find filename is 3
                     fd = open(fileName, O_WRONLY | O_APPEND | O_CREAT, S_IREAD | S_IWRITE);
                     fileNum = STDERR_FILENO;
-                }
-                if (fd < 0) {
-                    fprintf(stderr, "Can't open file %s: %s\n", fileName, strerror(errno));
-                    exit(1);
                 }
 
-                // Dup files and close dangling descriptors
-                if (dup2(fd, fileNum) < 0) {
-                    fprintf(stderr, "Can't dup2 %s to fd %d: %s\n", fileName, fileNum, strerror(errno));
-                    exit(1);
-                }
-                close(fd);
+                if (fd < 0) //Exits when error in opening fileName file
+                    specialExit("1", "Error with opening file, %s: %s", fileName, strerror(errno));
+
+                if (dup2(fd, fileNum) < 0)
+                    specialExit("1", "Error with dup2 %s to fd %d: %s\n", fileName, fileNum, strerror(errno));
+
+                close(fd); //Closes original file descriptor
             }
 
-            execvp(rawArgV[0], cmdArgV);
+            execvp(rawArgV[0], cmdArgV); //executes command
             free(rawArgV);
             break;
         }
         case -1:
-            specialExit("1", "Fork failed: %s\n", strerror(errno));
-            break;
+            specialExit("1", "Fork failed: %s\n", strerror(errno)); //Fork failed. Exit
         default:
-            if (wait3(&status, 0, &resUsage) < 0)
+            if (wait3(&exitStat, 0, &resUsage) < 0) //Wait for process to change state
                 specialExit("1", "Error with wait3: %s\n", strerror(errno));
 
-            if (gettimeofday(&endTime, NULL) < 0)
+            if (gettimeofday(&endTime, NULL) < 0) //Find ending time
                 specialExit("1", "Error with getting end time: %s\n", strerror(errno));
 
             double rTimeDiff, uTimeDiff, sTimeDiff;
 
-
+            //Calculate and print different run times
             rTimeDiff = (((endTime.tv_sec - startTime.tv_sec) * 1000000 + endTime.tv_usec) - startTime.tv_usec) /
                         1000000.0;
             uTimeDiff = (resUsage.ru_utime.tv_sec * 1000000L + resUsage.ru_utime.tv_usec) / 1000000.0;
             sTimeDiff = (resUsage.ru_stime.tv_sec * 1000000L + resUsage.ru_stime.tv_usec) / 1000000.0;
 
-            printf("\n[Finished with exit code %d]\n", WEXITSTATUS(status));
+            printf("\n[Finished with exit code %d]\n", WEXITSTATUS(exitStat));
             printf("Real: %03lfs, User: %03lfs, System: %03lfs\n", rTimeDiff, uTimeDiff, sTimeDiff);
 
     }
 }
 
 void execCmd(char *line) {
-    int count = 0;
+    int count = 0; //Number of arguments
     char **rawArgV = getArgs(line, &count);
     int cmdArgC = getCmdArgC(rawArgV, count);
     char **cmdArgV = (char **) malloc((cmdArgC + 1) * sizeof(char *));
@@ -166,32 +149,30 @@ void execCmd(char *line) {
     if (cmdArgC > 0) {
         int kk;
         for (kk = 0; kk <= cmdArgC; kk++)
-            cmdArgV[kk] = rawArgV[kk]; // Add arg to cmdArgV
+            cmdArgV[kk] = rawArgV[kk]; //Add raw arguments to command arguments
         cmdArgV[cmdArgC] = 0; //Add null terminator
     }
 
-    int redirArgC = getRedirArgC(rawArgV, count);
+    int redirArgC = getRedirArgC(rawArgV, count); //Number of redirections
     char **redirArgV = (char **) malloc((redirArgC + 1) * sizeof(char *));
     if (redirArgC > 0) {
         int kk;
         for (kk = 0; kk <= redirArgC; kk++)
-            redirArgV[kk] = rawArgV[kk + cmdArgC];
+            redirArgV[kk] = rawArgV[kk + cmdArgC]; //Get redirection arguments
     }
 
-    if (rawArgV == NULL) // if line is empty skip line
+    if (rawArgV == NULL) // If line is empty skip line
         return;
 
 
-#if(DEBUG_MODE==1)
+#if(DEBUG_MODE == 1) //If Debug mode is on (1), specifiy the below
 
     printf("Executing command %s with arguments \"", rawArgV[0]);
 
     if (cmdArgC > 0) {
         int kk;
-
         for (kk = 1; kk < cmdArgC; kk++) {
             printf("%s", cmdArgV[kk]);
-
             if (cmdArgC - kk > 1 || redirArgC > 0)
                 printf(", ");
         }
@@ -206,11 +187,12 @@ void execCmd(char *line) {
         }
     }
     printf("\"\n");
+
 #endif
 
-    forkProcess(redirArgC, redirArgV, rawArgV, cmdArgV);
+    forkProcess(redirArgC, redirArgV, rawArgV, cmdArgV); //Fork with given parameters
 
-    free(rawArgV);
+    free(rawArgV); //Free up all malloced things
     free(cmdArgV);
     free(redirArgV);
 }
@@ -227,11 +209,11 @@ int main(int argc, char **argv) {
     printf("\nkriSh $ ");
 
     while (getline(&line, &len, fp) != -1) {
-        if (line[0] == '#') //gets rid of comments
+        if (line[0] == '#') //Gets rid of comments
             continue;
         if (!strcmp(line, "exit\n"))
             break;
-        execCmd(line); //executes each command
+        execCmd(line); //Executes each command
         printf("\nkriSh $ ");
     }
     free(line);
